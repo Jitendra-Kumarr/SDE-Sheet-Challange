004f22a3c73ba3dedddc9f5b5f80f3862abaa45e
Jitendra Kumar <kr.jitendra7979@gmail.com>
2026-06-07 16:49:21 +0530

    Day 7 Completed

---
+++ Day7/TwoSum.java
@@ -0,0 +1,30 @@
+// package Day7;
+
+import java.util.Scanner;
+
+public class TwoSum {
+    public static int[] twoSum(int []arr, int target){
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]+arr[j] == target){
+                    return new int[]{i,j}; 
+                }
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+
+        int arr[] = new int[n]; 
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt(); 
+
+        int result[] = twoSum(arr, target); 
+
+        System.out.println("["+ result[0] + "," + result[1]+"]");
+    }
+}
